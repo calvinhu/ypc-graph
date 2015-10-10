8bab8f7dc1e1840cd3498d4b311812086101620b
Calvin Hu <calvinhu00@gmail.com>
2015-10-10 12:08:14 -0700

    better tables, moved second graph button to below the second graph when it appears

--- static/js/util.js
+++ static/js/util.js
@@ -30,3 +30,20 @@ function objToList(inputObject) {
 	}
 	return result;
 }
+
+function resizeTable(tableString) {
+	// Change the selector if needed
+	var $table = $(tableString),
+		$bodyCells = $table.find('tbody tr:first').children(),
+		colWidth;
+
+	// Get the tbody columns width array
+	colWidth = $bodyCells.map(function() {
+		return $(this).width();
+	}).get();
+
+	// Set the width of thead columns
+	$table.find('thead tr').children().each(function(i, v) {
+		$(v).width(colWidth[i]);
+	}); 
+}
