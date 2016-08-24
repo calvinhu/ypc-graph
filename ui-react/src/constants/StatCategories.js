70a66ca7dcd74cffbfae26146b5fd32a114926c4
Calvin Hu <calvinhu00@gmail.com>
2016-08-24 16:49:42 -0700

    remove react code, adding compiled UI code

--- ui-react/src/constants/StatCategories.js
+++
@@ -1,8 +0,0 @@
-// type of stats - different metrics per category
-export const RUSHING = 'rushing';
-export const RECEIVING = 'receiving';
-
-//type of play
-export const PASS = 'PASS'
-export const RUSH = 'RUSH'
-export const INCOMPLETE = 'INCOMPLETE'
