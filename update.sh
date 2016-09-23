bc23b148156c4f907971db58eacb4b5a21eb89ce
Calvin Hu <calvinhu00@gmail.com>
2016-09-23 12:42:22 -0700

    will hopefully be able to run update.sh from heroku scheduler

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl http://127.0.0.1:5000/api/v0/toprushers/2016/100 > ~/Workspace/ypc-graph/static/data/2016/rushing.json
-curl http://127.0.0.1:5000/api/v0/topreceivers/2016/100 > ~/Workspace/ypc-graph/static/data/2016/receiving.json
+curl http://ypcgraph.com/api/v0/toprushers/2016/100 > static/data/2016/rushing.json
+curl http://ypcgraph.com/api/v0/topreceivers/2016/100 > static/data/2016/receiving.json
