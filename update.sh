a8bf1e0f94a20c4c4d5a982db2e1da44ef57da05
Calvin Hu <calvinhu00@gmail.com>
2016-09-26 15:19:53 -0700

    update curl for updating top 100 list

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl http://ypcgraph.com/api/v0/toprushers/2016/100 > static/data/2016/rushing.json
-curl http://ypcgraph.com/api/v0/topreceivers/2016/100 > static/data/2016/receiving.json
+curl http://www.ypcgraph.com/api/v0/toprushers/2016/100 > static/data/2016/rushing.json
+curl http://www.ypcgraph.com/api/v0/topreceivers/2016/100 > static/data/2016/receiving.json
