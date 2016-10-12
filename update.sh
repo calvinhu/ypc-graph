d989f136c71ddc4ee0c252f60b51cba4236ad263
Calvin Hu <calvinhu00@gmail.com>
2016-10-11 18:11:32 -0700

    using firebase API for 2016 season

--- update.sh
+++ update.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-curl http://www.ypcgraph.com/api/v0/toprushers/2016/100 > static/data/2016/rushing.json
-curl http://www.ypcgraph.com/api/v0/topreceivers/2016/100 > static/data/2016/receiving.json
+RUSHING=$(curl http://www.ypcgraph.com/api/v0/toprushers/2016/100)
+RECEIVING=$(curl http://www.ypcgraph.com/api/v0/topreceivers/2016/100)
+
+# echo $RUSHING
+# echo $RECEIVING
+
+# perl -pi -e 's/"result"/"rushing"/g' static/data/2016/rushing.json 
+# perl -pi -e 's/"result"/"receiving"/g' static/data/2016/receiving.json 
+
+curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X PUT --data "$RUSHING" "https://ypcgraph-7be8c.firebaseio.com/rushing.json"
+
+curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X PUT --data "$RECEIVING" "https://ypcgraph-7be8c.firebaseio.com/receiving.json"
+# curl -H "Content-Type: application/json" -X PUT -d $RECEIVING https://ypcgraph-7be8c.firebaseio.com/receiving.json
